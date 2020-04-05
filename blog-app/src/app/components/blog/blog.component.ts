3ec4c0cc1ef08f0854e261a6b2ced6b1af187a7a
Khroum <fladro224@gmail.com>
2020-04-05 15:06:32 +0200

    lab 6

--- blog-app/src/app/components/blog/blog.component.ts
+++ blog-app/src/app/components/blog/blog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-blog',
+  selector: 'blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
+  @Input() filterText: string;
+  public items$: any;
 
-  constructor() { }
+  constructor(private service: DataService) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  getAll() {
+    this.service.getAll().subscribe((response) => {
+      this.items$ = response;
+    });
+  }
 }
